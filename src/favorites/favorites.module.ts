60c6368387084b435809372e22c0eadbb9682447
Eugene Gatsak <greenglaz@gmail.com>
2023-02-20 15:34:37 +0400

    fix: fix linter warnings

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
-import { DbModule } from 'src/db/db.module';
 import { ArtistsModule } from 'src/artists/artists.module';
 import { AlbumsModule } from 'src/albums/albums.module';
 import { TracksModule } from 'src/tracks/tracks.module';
